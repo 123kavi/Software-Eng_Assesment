6c2bec4e3442066d139b0143befb0832ff54141e
123kavi <83937185+123kavi@users.noreply.github.com>
2022-07-26 16:12:33 +0530

    7.create user components in frontend

---
+++ Frontend/src/components/CreateUser.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import "../components/CSS/cre.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function CreateUser() {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setUser((values) => ({
+      ...values,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("user", user);
+
+    try {
+      const response = await axios.post("user/createUser", user).then(() => {
+        alert("User Created Successfully");
+        navigate("/");
+      });
+
+      navigate("/");
+    } catch (error) {
+      alert(error.response.data.msg);
+    }
+  };
+  return (
+    <div>
+          <div className="main2">
+          <div className="sub-main2">
+          <div className="photo">
+          <img src="https://media.istockphoto.com/vectors/system-administration-concept-vector-illustration-vector-id1002244908?b=1&k=20&m=1002244908&s=612x612&w=0&h=3X6nTWj8ThA2d1HXGRYRbYCdj4kNNVCFAvD4IDncYBM=" />
+        </div>
+        <h1>
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          <b>
+          Create User</b></h1>
+        <form onSubmit={handleSubmit}>
+          <p>First Name</p>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="Enter Firstname"
+            onChange={handleChange}
+            required
+          />
+          <p>Last Name</p>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Enter Lastname"
+            onChange={handleChange}
+            required
+          />
+          <p>Email</p>
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+            required
+          />
+          <p>User Type</p>
+          <div className="dropdown">
+            <select
+              class="form-control"
+              id="exampleFormControlSelect1"
+              name="accountType"
+              onChange={handleChange}
+              required
+            >
+              <option>Select Type</option>
+              <option value="admin">Admin</option>
+              <option value="student">Student</option>
+            </select>
+          </div>
+
+          <div>
+            <button
+              className="btnSubmit mt-3"
+              style={{ marginTop: "15px" }}
+              type="submit"
+              name="submit"
+            >
+              CREATE
+            </button>
+          </div>
+        </form>
+        </div>
+        </div>
+    </div>
+  );
+}
